a8099ec06537128ff0bae449809c11ffeee898aa
lionhairdino <lionhairdino@users.noreply.github.com>
2025-02-23 11:45:09 +0000

    deploy: lionhairdino/siteSource@e18b8bf0cd3a0b9e7d1c87f9923aeb6312b6b7e4

--- script/darkmode.js
+++ script/darkmode.js
@@ -1,23 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleButton = document.getElementById('theme-toggle');
-  const body = document.body;
-
-  // 로컬 스토리지에서 테마 상태 확인
   const savedTheme = localStorage.getItem('theme');
+
   if (savedTheme === 'dark') {
-    body.classList.add('dark-mode');
-    toggleButton.textContent = '라이트 모드';
+    document.documentElement.classList.add('dark-mode');
+    toggleButton.textContent = 'LIGHT';
   }
 
   toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-
-    if (body.classList.contains('dark-mode')) {
-      toggleButton.textContent = '라이트 모드';
+    const elem = document.documentElement.classList;
+    elem.toggle('dark-mode');
+    if (elem.contains('dark-mode')) {
       localStorage.setItem('theme', 'dark');
+      toggleButton.textContent = 'LIGHT';
     } else {
-      toggleButton.textContent = '다크 모드';
       localStorage.setItem('theme', 'light');
+      toggleButton.textContent = 'DARK';
     }
   });
 });
